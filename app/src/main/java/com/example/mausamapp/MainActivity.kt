0c22f272d1564c90dd750f9c39ad13cd5f5f55ec
Ansul Singh <ansulsingh@shubham-bhaskar.local>
2021-03-02 17:30:18 +0530

    space added

--- app/src/main/java/com/example/mausamapp/MainActivity.kt
+++ app/src/main/java/com/example/mausamapp/MainActivity.kt
@@ -39,6 +39,11 @@ class MainActivity: AppCompatActivity() {
 
 
 
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
